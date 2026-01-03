4fd37ba2c6b459b4640b662f16b20281dac52dae
CoolCoolTomato <2983315455@qq.com>
2026-01-04 03:20:21 +0800

    add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AppRouter } from '@/router'
-import './App.css'
+import { ThemeProvider } from '@/components/custom/theme-provider'
 
 function App() {
   const basename = import.meta.env.BASE_URL || '/'
 
   return (
-    <Router basename={basename}>
-      <AppRouter />
-    </Router>
+    <div className="font-sans antialiased" style={{ fontFamily: 'var(--font-inter)' }}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <Router basename={basename}>
+          <AppRouter />
+        </Router>
+      </ThemeProvider>
+    </div>
   )
 }
 
